5fc6a21b4338d65f6820e468edc2c67ade79d3c5
Rakhi-kashyap <101251859+Rakhikahyap@users.noreply.github.com>
2022-07-29 23:56:53 +0530

    Add files via upload

---
+++ three.js
@@ -0,0 +1,10 @@
+
+let readlineSync = require("readline-sync")
+var varx=readlineSync.questionInt("enter a number==")
+var vary=readlineSync.questionInt("enter a number1==")
+if(varx % vary==0){
+  console.log("divisible")
+}
+else{
+  console.log("not divisible")
+}
