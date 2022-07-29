26254184e236058159e444dee3e586156f7c166c
Rakhi-kashyap <101251859+Rakhikahyap@users.noreply.github.com>
2022-07-29 23:47:22 +0530

    Add files via upload

---
+++ five .js
@@ -0,0 +1,24 @@
+// a= int(input('enter any number'))
+// b= int(input('enter any number'))
+// c=int (input('enter any number'))
+// if a<b and a<c:
+// 	print(' middle number a')
+// elif b<c and b<a:
+// 	print(' middle number b')
+// else:
+// 	print(' middle nu
+
+
+
+// var a=require("readline-sync")
+// var b=a.questionInt("enter num:")
+// var c=a.question("enter string")
+// console.log(b)
+// console.log(c)
+// 
+
+var a=require('readline-sync')
+var b=a.questionInt('enter num;')
+var c=a.question("enter num")
+console.log(b)
+console.log(c)
