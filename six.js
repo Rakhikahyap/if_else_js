4f21c054d3aa560430f07c676e67b360bdef4225
Rakhi-kashyap <101251859+Rakhikahyap@users.noreply.github.com>
2022-07-29 23:53:20 +0530

    Add files via upload

---
+++ six.js
@@ -0,0 +1,35 @@
+// monument=input('enter any monument')
+// if monument=='Red fort':
+// 	print('Delhi')
+// elif monument=='Taj Mahal':
+// 	print('Agra')
+// elif monument=='Jai Mahal':
+// 	print('Jaipur')
+// else:
+// 	print('Nothing')
+
+var x=require('readline-sync').questionInt('enter the name')
+if (x=='Red fort'){
+    console.log(delhi)
+}
+else if (x=='taj Mahal'){
+    console.log(Agra)
+}
+else if (x=='jay Mahal'){
+    console.log(jaipur)
+}
+else{
+    console.log(nothing)
+}
+
+
+// let readlineSync = require("readline-sync");
+// var number=readlineSync.questionInt("enter a number==");
+// if (number == 54){
+//   console.log("valid") 
+// }
+// else{
+//   console.log("not")
+// }
+
+
