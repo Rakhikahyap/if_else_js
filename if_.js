80ee475922b08d25542981541c1e0dcd18eaf6d9
Rakhi-kashyap <101251859+Rakhikahyap@users.noreply.github.com>
2022-07-29 23:48:58 +0530

    Add files via upload

---
+++ if_.js
@@ -0,0 +1,14 @@
+// monument=input('enter any monument')
+// if monument=='Red fort':
+// 	print('Delhi')
+// elif monument=='Taj Mahal':
+// 	print('Agra')
+// elif monument=='Jai Mahal':
+// 	print('Jaipur')
+// else:
+// 	print('Nothing')
+
+var monument=require('readline-sync').question("enter the monument")
+if (monument=='red fort'){
+    console.log('delhi')
+}
