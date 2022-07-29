1d33b4e617251adec99d687e8d6d77814d700cee
Rakhi-kashyap <101251859+Rakhikahyap@users.noreply.github.com>
2022-07-29 23:57:17 +0530

    Add files via upload

---
+++ two.js
@@ -0,0 +1,10 @@
+let x=require('readline-sync').questionInt('enter the numbre');
+if (x>0){
+    console.log("positive")
+}
+else if (x<0){
+    console.log("nagative")
+}
+else{
+    console.log("zero")
+}
